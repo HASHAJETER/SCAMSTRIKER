65c14829ce3a23272c563a6b9ec92297d4f7bfb7
AlphaxHotelxMikexEchoxDelta <95902084+AlphaxHotelxMikexEchoxDelta@users.noreply.github.com>
2024-03-23 14:08:53 +0100

    Update pgg.py

--- pgg.py
+++ pgg.py
@@ -12,8 +12,17 @@ with open('mail.eml', 'rb') as fhdl:
 
 ep = eml_parser.EmlParser()
 
-print(eml_parser)
-
 parsed_eml = ep.decode_email_bytes(raw_email)
 
-print(json.dumps(parsed_eml, default=json_serial, indent=2))
+#print(json.dumps(parsed_eml["header"], default=json_serial, indent=2))
+
+for p in parsed_eml["header"]:
+  if p in ["subject", "from", "to", "received_domain", "received_ip" ]:
+    print(p, "->", parsed_eml['header'][p])
+
+print( "\n\n--- Attachement ---" )
+for i in parsed_eml["attachment"]:
+  print( "\n\tfilename ->", i["filename"] )
+
+  for s in i["hash"].keys():
+    print( s, "->", i["hash"][s] )
